c5da505586c79e048cab78d70223cf57af40718e
James Simas <james.r.simas@gmail.com>
2019-09-15 12:53:14 -0700

    Add vars and s3 example

--- 01-hello_world/hello_world.tf
+++ 01-hello_world/hello_world.tf
@@ -1,9 +1,23 @@
 provider "aws" {
   profile    = "default"
-  region     = "us-west-1"
+  region     = var.region
 }
+
+resource "aws_eip" "ip" {
+    vpc = true
+    instance = aws_instance.example.id
+}
+
+#resource "aws_s3_bucket" "example" {
+#  bucket = "terraform-getting-started-guide-848192394"
+#  acl    = "private"
+#}
 
 resource "aws_instance" "example" {
   ami           = "ami-00513ac407df1a26b"
   instance_type = "t2.micro"
+  #depends_on = [aws_s3_bucket.example]
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
 }
